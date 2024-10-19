4d7fe2e0773d12f04aa9dd66ae2c57bfa95d04e3
sanals <5sanal5z5@gmail.com>
2024-10-19 18:16:06 +0530

    dd

---
+++ src/main/java/com/slnm/microaddr/MicroserivesSeleniumAddressApplication.java
@@ -0,0 +1,13 @@
+package com.slnm.microaddr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserivesSeleniumAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserivesSeleniumAddressApplication.class, args);
+	}
+
+}
